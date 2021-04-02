22083ca78e49bf544e871d6369a1b6cd544e5961
Gopi M Tatiraju <deathcoderx@gmail.com>
2021-04-03 00:05:42 +0530

    Update benchmark.cpp

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,5 +1,8 @@
-#include "rapidcsv.h"
-#include "csv.h"
+// use the above line everytime
+// export LD_LIBRARY_PATH="/home/heisenbug/Dev/mlpack/mlpack-lib/build/lib/:$LD_LIBRARY_PATH"
+
+#include <rapidcsv.h>
+#include <csv.h>
 #include <iostream>
 #include <vector>
 #include <mlpack/core.hpp>
@@ -10,7 +13,7 @@ void create_csv(size_t n_rows, size_t n_cols)
 {
   arma::mat matrix(n_rows, n_cols);
   matrix.randu();
-  matrix.save("test.csv", arma::file_type::csv_ascii);
+  matrix.save("data/test.csv", arma::file_type::csv_ascii);
 }
 
 int main()
@@ -21,10 +24,10 @@ int main()
   arma::vec col_sizes = {5, 15, 25};
 
   arma::mat log;
-  log.set_size(row_sizes.n_elem * col_sizes.n_elem, 5);
+  log.set_size(row_sizes.n_elem * col_sizes.n_elem, 6);
   arma::mat test;
 
-  std::cout << "Total combinations: " << log.n_rows << '\n';
+  std::cout << "Total combinations: " << test.n_rows << '\n';
 
   int counter = 0;
   for (int i = 0; i < row_sizes.n_elem; i++)
@@ -33,14 +36,16 @@ int main()
     {
       std::cout << "Count: " << counter << '\n';
 
+      // log row size and column size
       log(counter, 0) = row_sizes(i);
       log(counter, 1) = col_sizes(j);
 
+      // creating test.csv
       create_csv(row_sizes(i), col_sizes(j));
 
       // Load using default csv parser
       auto start = std::chrono::high_resolution_clock::now();
-      test.load("test.csv", arma::file_type::csv_ascii);
+      test.load("data/test.csv", arma::file_type::csv_ascii);
       auto stop = std::chrono::high_resolution_clock::now();
       auto time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
       log(counter, 2) = time_taken.count();
@@ -48,30 +53,57 @@ int main()
       // Load using mlpack's custom csv parser
       mlpack::data::DatasetInfo info;
       start = std::chrono::high_resolution_clock::now();
-      mlpack::data::Load("test.csv", test, info, false, true);
+      mlpack::data::Load("data/test.csv", test, info, false, true);
       stop = std::chrono::high_resolution_clock::now();
       time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
       log(counter, 3) = time_taken.count();
 
       // Load using rapidcsv
       start = std::chrono::high_resolution_clock::now();
-      rapidcsv::Document doc("test.csv", rapidcsv::LabelParams(-1, -1));
-      arma::mat data(doc.GetRowCount(), doc.GetColumnCount(), arma::fill::ones);
-      std::vector<float> col;
-      for (int i = 0; i < doc.GetRowCount(); i++)
+      // Intialize without header
+      rapidcsv::Document doc_rapid_csv("data/test.csv", rapidcsv::LabelParams(-1, -1));
+      // is it safe or recommonded to fill mat at the time of initialization?
+      arma::fmat mat_rcsv(doc_rapid_csv.GetRowCount(), doc_rapid_csv.GetColumnCount());
+      std::vector<float> column;
+
+      for(int i = 0; i < doc_rapid_csv.GetColumnCount(); i++)
       {
-        col = doc.GetRow<float>(i);
-        for (int j = 0; j < doc.GetColumnCount(); j++)
-        {
-          data(i, j) = col[j];
-        }
+        column = doc_rapid_csv.GetColumn<float>(i);
+        arma::fvec column_vector(column);
+        mat_rcsv.col(i) = column_vector;
       }
+
       stop = std::chrono::high_resolution_clock::now();
       time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
       log(counter, 4) = time_taken.count();
 
+      // Load using fastcsv
+      start = std::chrono::high_resolution_clock::now();
+      io::CSVReader<4> doc_fast_csv("data/test.csv");
+      std::vector<float> row_elements;
+      std::stringstream row_ss;
+      arma::fmat mat_fcsv(row_sizes(i), col_sizes(j));
+      int i = 0;
+      while(char*line = doc_fast_csv.next_line())
+      {
+        std::stringstream row_ss(line);
+        
+        while (row_ss.good()) {
+          std::string substr;
+          getline(row_ss, substr, ',');
+          row_elements.push_back(std::stod(substr));
+        }
+        arma::frowvec row_vector(row_elements);
+        row_elements.clear();
+        mat_fcsv.row(i) = row_vector;
+        i++;
+      }
+      stop = std::chrono::high_resolution_clock::now();
+      time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+      log(counter, 5) = time_taken.count();
+
       counter++;
     }
   }
-  log.save("log.csv", arma::file_type::csv_ascii);
+  log.save("logs/log.csv", arma::file_type::csv_ascii);
 }
